88fa06fdca386a213281833064b842e066ea44ad
Scott Richards <scottrichards789@gmail.com>
2023-08-15 00:53:02 +0100

    Added product schema. Removed unused schemas

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,10 +1,6 @@
-import { type SchemaTypeDefinition } from 'sanity'
-
-import blockContent from './schemas/blockContent'
-import category from './schemas/category'
-import post from './schemas/post'
-import author from './schemas/author'
+import { type SchemaTypeDefinition } from "sanity";
+import product from "./schemas/product";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, author, category, blockContent],
-}
+  types: [product],
+};
